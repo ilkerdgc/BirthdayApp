005783a641bbcd3022da0e1f3b3aba89f9f66d1b
ilkerdgc <ilker.dagcii@gmail.com>
2018-12-23 12:58:37 +0300

    BirthdayApp

---
+++ BirthdayApp/BirthdayApp/Controllers/DavetiyeController.cs
@@ -0,0 +1,35 @@
+﻿using BirthdayApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BirthdayApp.Controllers
+{
+    public class DavetiyeController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<DavetiyeModel> Katilanlar()
+        {
+            return Veritabanı.Liste.Where(x => x.KatilmaDurumu == true);
+        }
+
+        [HttpGet]
+        public IEnumerable<DavetiyeModel> Katilmayanlar()
+        {
+            return Veritabanı.Liste.Where(x => x.KatilmaDurumu == false);
+        }
+
+        [HttpPost]
+        public void Ekle(DavetiyeModel davetiyeModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Veritabanı.Add(davetiyeModel);
+            }
+            
+        }
+    }
+}
